3ccde85db6bf791981a7fe9d8b784ecc38a6dfd6
learn-rails learner <ipartbook@hotmail.com>
2013-11-30 12:29:43 +0000

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,12 @@ class VisitorsController < ApplicationController
     @owner = Owner.new
     Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
     Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
-    render 'visitors/new', :layout => 'special'
+
+    #flash[:notice] = 'Welcome!'
+    #flash[:alert] = 'My Birthday is soon.'
+    #flash[:warning] = 'test warning'
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My Birthday is soon.'
+    render 'visitors/new'
   end
 end
