6f3c7c0ad8d5ce02dfaa4d6bf66da92588c08012
learn-rails learner <ipartbook@hotmail.com>
2013-11-30 11:02:38 +0000

    log.debug AND lib/example.rb

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,9 @@
 class VisitorsController < ApplicationController
   def new
+    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
+    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+    Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
     render 'visitors/new'
   end
 end
