3c7621b75ca1ddb8db699a729d5aa3fa9b1e9064
learn-rails learner <ipartbook@hotmail.com>
2013-11-30 11:49:02 +0000

    try sidebar, view include other view

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,6 @@ class VisitorsController < ApplicationController
     @owner = Owner.new
     Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
     Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
-    render 'visitors/new'
+    render 'visitors/new', :layout => 'special'
   end
 end
